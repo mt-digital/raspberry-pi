e0169b35b446671787a54d9a94af8d798b9a9410
Matthew Turner <maturner01@gmail.com>
2015-03-05 20:59:43 -0800

    see if it serves from rpi

--- web-leds/app.py
+++ web-leds/app.py
@@ -11,16 +11,10 @@ def index():
 def toggle_led(led_idx):
     """Show the buttons!"""
 
-    #if not request.json or 'ledIdx' not in request.json:
-        #led_idx = None
-    #else:
-        #led_idx = request.json['ledIdx']
-    #if led_idx:
-
     return render_template('index.html', led_idx=led_idx)
 
 def _toggle_led(led_idx):
-    pass
+    print "not really toggling " + led_idx
 
 #class LEDButtonsForm(Form):
     #"""Buttons to make the LEDs light up!
